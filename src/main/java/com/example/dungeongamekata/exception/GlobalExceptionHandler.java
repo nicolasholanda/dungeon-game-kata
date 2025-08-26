75aec7bcc66662fdfe76db531fe0b44888adcc5d
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-08-26 10:26:22 -0300

    added actuator

--- src/main/java/com/example/dungeongamekata/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/dungeongamekata/exception/GlobalExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-@ControllerAdvice
+@ControllerAdvice(basePackages = "com.example.dungeongamekata.controller")
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(InvalidDungeonInputException.class)
